c2eb2b45c9f43c95917194ce9ac3ec76a7097863
_vitormuller <vitormuller66@gmail.com>
2022-01-10 20:38:23 -0300

    asdadasda

--- src/components/TelaVideo/index.js
+++ src/components/TelaVideo/index.js
@@ -5,9 +5,8 @@ import "./style.css"
 function TelaVideo(){
     return(
         <Container>
-            <video width="80%" controls="controls" autoPlay="autoplay">
-                <source src="https://youtu.be/Qn4vWZr3zC0" type="video/mp4"></source>
-            </video>
+                <iframe width="560" height="315" src="https://www.youtube.com/embed/Qn4vWZr3zC0" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+       
             <Texto> Após assistir o video, responda o formulário pufavô!</Texto>
             <a href="https://docs.google.com/forms/d/e/1FAIpQLSeVAwAkn80aDBcEZoujbv75Te-l6wyiXi3meqGmepJwpzA9eA/viewform?usp=sf_link">ACESSE AQUI!</a>
         </Container>
